1fbcb344780b913c278cad47007640509e2ddb56
Jayson Zhou <jaysonz6879@gmail.com>
2022-03-22 00:36:49 +0000

    [Add] add Query1b.sql.

---
+++ HiveQL_Queries/Query1b.sql
@@ -0,0 +1,8 @@
+select ss_item_sk, sum(ss_quantity) as sales from store_sales_40
+where ss_sold_date_sk >= 2451146
+  and ss_sold_date_sk <= 2452268
+  and ss_item_sk is not null
+  and ss_quantity is not null
+group by ss_item_sk
+order by sales desc
+    limit 10;
