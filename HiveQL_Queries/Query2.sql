3d2ae27f6026e547984608d644cb2eb5d5ab13b4
Jayson_Z <55767546+Jayson0709@users.noreply.github.com>
2022-04-22 22:27:21 +0100

    [Fix] adjust the format of Query2.sql

--- HiveQL_Queries/Query2.sql
+++ HiveQL_Queries/Query2.sql
@@ -1,5 +1,5 @@
-select S.s_store_sk, SS.total_net_paid, S.s_floor_space from store_40 S
-                                                                 join (
+select S.s_store_sk, SS.total_net_paid, S.s_floor_space from store_40 S join
+(
     select ss_store_sk, sum(ss_net_paid) as total_net_paid from store_sales_40
     where ss_sold_date_sk >= 2451146
       and ss_sold_date_sk <= 2452268
@@ -7,6 +7,6 @@ select S.s_store_sk, SS.total_net_paid, S.s_floor_space from store_40 S
       and ss_net_paid is not null
     group by ss_store_sk
 ) SS
-                                                                      on S.s_store_sk = SS.ss_store_sk
+on S.s_store_sk = SS.ss_store_sk
 order by S.s_floor_space desc, SS.total_net_paid desc
-    limit 10;
+limit 10;
